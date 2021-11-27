7d2ee1db366fc370fe154bec6bbb3f2f9fe0c888
Reid Elleman <reidLMN@gmail.com>
2021-11-27 11:37:37 -0500

    Update LivingRoomLights.ino
    Added twinkle and scroll

--- LivingRoomLights/LivingRoomLights.ino
+++ LivingRoomLights/LivingRoomLights.ino
@@ -2,13 +2,16 @@
 
 int blue_primary = 0;
 int blue_secondary = 0;
+int blue_tertiary = 255;
 int green_primary = 0;
 int green_secondary = 255;
+int green_tertiary = 0;
 int len_LED_strip = 200;
 int len_LED_segments = 20;
 int pin_LED_data = 3;
 int red_primary = 255;
 int red_secondary = 0;
+int red_tertiary = 0;
 
 Adafruit_NeoPixel pixels(len_LED_strip, pin_LED_data, NEO_GRB + NEO_KHZ800);
 
@@ -18,7 +21,9 @@ void setup() {
 }
 
 void loop() {
-  animateScrollingBars(20, 500);
+  for (int i; i<5; i++){
+    animateScrollingBars(20, 500);
+  }
 }
 
 void setWholeStrip(int r, int g, int b){
@@ -34,11 +39,29 @@ void setZone(int r, int g, int b, int l, int u){
 }
 
 void animateScrollingBars(int w, int s){
+  int num_segments = len_LED_strip/(2*w);
   for(int i=0; i<w; i++){
-      setZone(red_primary, green_primary, blue_primary, i, i+w);
-      setZone(red_secondary, green_secondary, blue_secondary, i+w, i+(2*w));
-      pixels.show();
-      delay(s);
+    for(int j=0; j<=num_segments; j++){
+      setZone(red_primary, green_primary, blue_primary, i+(j*w), i+((j+1)*w));
+      setZone(red_secondary, green_secondary, blue_secondary, i+((j+1)*w), i+((j+2)*w));
+    }
+    pixels.show();
+    delay(s);
+  }
+}
+
+void animateTwinkle(int s, int n, int p){
+  for(int i=0; i<len_LED_strip; i++){
+    int mod = (i+p)%n;
+    if (mod==0){
+      pixels.setPixelColor(i, pixels.Color(red_primary, green_primary, blue_primary));
+    }
+    else if (mod==0){
+      pixels.setPixelColor(i, pixels.Color(red_secondary, green_secondary, blue_secondary));
+    }
+    else if (mod==0){
+      pixels.setPixelColor(i, pixels.Color(red_tertiary, green_tertiary, blue_tertiary));
+    }
   }
 }
 
