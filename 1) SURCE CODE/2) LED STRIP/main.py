773f6464f9b50cac7c89550fc6c44cb04e6910c7
reidlmn19 <reidlmn@gmail.com>
2022-01-15 10:29:00 -0500

    Added alexa template, color lookup function

--- 1) SURCE CODE/2) LED STRIP/main.py
+++ 1) SURCE CODE/2) LED STRIP/main.py
@@ -2,7 +2,29 @@ import os
 import pandas as pd
 
 main_directory = os.path.dirname(os.path.realpath(__file__))
-path_colorLib = '/'.join([main_directory,'CrayolaColorLibrary.csv'])
+color_str = 'Macaroni and Cheese'
 
-df_colorLib = pd.read_csv(path_colorLib, index_col="Name")
-print(df_colorLib)
+def set_LEDColor(color, lookup=None):
+    ind = str(color)
+    
+    if lookup:
+        df_colorLib = pd.read_csv(lookup, index_col="Name")
+    else:
+        path_colorLib = '/'.join([main_directory,'CrayolaColorLibrary.csv'])
+        df_colorLib = pd.read_csv(path_colorLib, index_col="Name")
+    
+    if ind in df_colorLib.index:
+        h_color = df_colorLib.loc[ind]['Hexidecimal']
+    else:
+        print('Color not found')
+        h_color = None
+    return h_color
+
+def hexToRGB(h):
+    r = int(h[0:2],16)
+    g = int(h[2:4],16)
+    b = int(h[4:6],16)
+    return [r,g,b]
+
+color_h = set_LEDColor(color_str)
+print(hexToRGB(color_h))
