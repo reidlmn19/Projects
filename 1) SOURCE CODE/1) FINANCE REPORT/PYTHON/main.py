848ae0e1f755158a2981d71861037d383652682a
SpaceBucket69420 <reidLMN@gmail.com>
2023-11-05 16:27:24 -0500

    get_pdf_author started

--- 1) SOURCE CODE/1) FINANCE REPORT/PYTHON/main.py
+++ 1) SOURCE CODE/1) FINANCE REPORT/PYTHON/main.py
@@ -3,15 +3,38 @@ from tabulate import tabulate
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import PyPDF2
 import os
+
+
+def get_pdf_author(path, show_all=False):
+    print(path)
+    matches = {
+        'Nelnet': NelnetStatement,
+        'Betterment': BettermentStatement
+    }
+    reader = PyPDF2.PdfReader(path)
+    author = reader.metadata.author
+    if author in matches:
+        return matches[author]
+
+    pages_text = ''
+    for page in reader.pages:
+        page_text = page.extract_text()
+        pages_text = pages_text + page_text
+    print(pages_text)
+
+
 
 if __name__ == '__main__':
     sample = 'Santander_Bank-Account_2021-04-24.pdf'
-    test_data = r'D:\OCT2023DATA'
+    test_data = r'E:\OCT2023DATA'
     r = FinanceManager(title="Sample Report - 20230620", path_rawdata=test_data)
-    f = f'{r.path_rawData}\\{r.file_manager.raw_data[0]}'
-    print(f)
 
-    # r.update_data(_print=True)
-    # r.analyst.data_coverage()
-    # r.analyst.file_results()
+    target = r'E:\OCT2023DATA\Betterment_401k_Quarterly_Statement_2023-09-30.pdf'
+    obj = get_pdf_author(target, show_all=True)
+    print(obj)
+
+    # for file in r.file_manager.raw_data:
+    #     f = f'{r.path_rawData}\\{file}'
+    #     get_file_author(f)
